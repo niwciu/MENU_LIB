34cc95c01f86dd0aed5c1712b9ea81366bac2389
Your Name <niwciu@gmail.com>
2025-11-17 10:49:09 +0100

    bug_fix: remove error line printing when finding null ptr as next menu object to print. It should just end printing objects

--- lib/MENU_LIB/menu_lib.c
+++ lib/MENU_LIB/menu_lib.c
@@ -69,7 +69,7 @@ static void clear_current_menu_view_with_cursor(void);
 static void update_menu_item_pointer_to_print(void);
 static void update_current_menu_view_with_cursor(void);
 static void display_cursor_marker_if_needed(uint8_t row);
-static void display_error_line(uint8_t row, const char *msg);
+// static void display_error_line(uint8_t row, const char *msg);
 static void display_menu_item_name(uint8_t row, const char *name);
 
 /* --- Implementation --- */
@@ -484,7 +484,7 @@ static void update_current_menu_view_with_cursor(void)
 
         if (menu_item_2_print == NULL)
         {
-            display_error_line(row, "NULL PTR FOUND");
+            // Null -> no more items to print on lcd 
             break;
         }
 
@@ -511,11 +511,11 @@ static void display_cursor_marker_if_needed(uint8_t row)
  * @param row Screen row to display error
  * @param msg Error message string
  */
-static void display_error_line(uint8_t row, const char *msg)
-{
-    DISPLAY->cursor_position(row, STRING_START_POSITION);
-    DISPLAY->print_string(msg);
-}
+// static void display_error_line(uint8_t row, const char *msg)
+// {
+//     DISPLAY->cursor_position(row, STRING_START_POSITION);
+//     DISPLAY->print_string(msg);
+// }
 
 /**
  * @brief Display the name of a menu item at the given row.
